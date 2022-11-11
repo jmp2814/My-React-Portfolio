13d146843977facece83daac121effa6a818fd97
jmp2814 <jmp2814@gmail.com>
2022-11-11 12:45:15 -0500

    initial commit

---
+++ my-react-portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(props) {
+    return (
+      <footer>
+        <a href="https://github.com/jmp2814">GitHub</a>
+        <a href="">LinkedIn</a>
+        <a href="">Twitter</a>
+      </footer>
+    );
+  }
+  
+  export default Footer;
